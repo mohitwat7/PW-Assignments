196108a6981a3eb2b8be564bacad9778ef8c54b0
Mohit Wat <116029995+mohitwat7@users.noreply.github.com>
2023-12-13 18:53:07 +0530

    Delete 4.js

--- 4.js
+++
@@ -1,5 +0,0 @@
-// Specify an example for all the datatypes in Javascript, store the values in a variable, and verify the type of
-// value stored.
-
-let variable1 = "I am happy to learn JS from PW Skills"
-console.log(typeof variable1)
