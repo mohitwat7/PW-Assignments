7b54eab744aba3350c5aa435f7a30ddb1a811cbf
Mohit Wat <116029995+mohitwat7@users.noreply.github.com>
2023-12-13 18:53:00 +0530

    Delete 2.js

--- 2.js
+++
@@ -1,3 +0,0 @@
-/* Create an array of 10 products that you have recently purchased or viewed on an e-commerce site.*/
-
-[ "Face Serum", "iPhone", "MacBook", "Dress", "Candles", "Shoes", "Car", "Toys", "Crockery",];
